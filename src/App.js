e395499e33453a8a619c21603d04b0a65573b86d
Abrahmson <hhy18519720840@gmail.com>
2019-08-13 08:20:51 +0800

    update router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import {HashRouter,BrowserRouter,Route,Switch} from "react-router-dom"
+
+import Login from "./pages/login/login"
+import Admin from "./pages/login/Admin"
+
 import logo from './logo.svg';
 import './App.css';
+import { from } from 'rxjs';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+     <HashRouter>
+       <Switch>
+         <Route path="/login" Component={Login}></Route>
+         <Route path="/admin" Component={Admin}></Route>
+       </Switch>
+     </HashRouter>
     )
   }
 }
